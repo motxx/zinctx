67a994387297f1481663c8f8f0b63bf077658d59
motxx <5776910+motxx@users.noreply.github.com>
2021-04-15 07:23:58 +0900

    add endpoint url comment

--- src/const/zandbox.rs
+++ src/const/zandbox.rs
@@ -1,3 +1,5 @@
+// See: https://www.notion.so/Endpoint-documentation-f9841e45616f4c0d85275cd72812b540
+
 pub static CONTRACT_URL = "/api/v1/contract";
 pub static CONTRACT_INITIALIZE_URL = "/api/v1/contract/initialize";
 pub static CONTRACT_QUERY_URL = "/api/v1/contract/query";
